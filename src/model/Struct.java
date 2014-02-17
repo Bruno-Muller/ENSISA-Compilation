264fc80cd670477859ccd2225e503d0a002d3ba1
Bruno Muller <muller.bru@gmail.com>
2014-02-17 13:14:29 +0100

    Update Struct & cup

--- src/model/Struct.java
+++ src/model/Struct.java
@@ -4,24 +4,46 @@ import java.util.HashMap;
 
 public class Struct {
 	
-	private HashMap<String, DataType> data;
+	private HashMap<String, DataType> dataType;
+	private HashMap<String, String> dataValue;
+	
 	private static HashMap<String, Struct> struct;
 	
 	public static Struct getStruct(String name) {
 		return Struct.struct.get(name);
 	}
 	
-	public Struct(String name) {
-		this.data = new HashMap<String, DataType>();
-		Struct.struct.put(name, this);
+	public static Struct createStructDefinition(String structDefinitionName) {
+		return new Struct(structDefinitionName);
 	}
 	
-	public void putVar(String s) {
-		this.data.put(s, DataType.VAR);
+	public static Struct createStruct(String structName, String structDefinitionName) {
+		return new Struct(structName, structDefinitionName);
 	}
 	
-	public void putPtr(String s) {
-		this.data.put(s, DataType.PTR);
+	private Struct(String structDefinitionName) {
+		this.dataType = new HashMap<String, DataType>();
+		this.dataValue = new HashMap<String, String>();
+		Struct.struct.put(structDefinitionName, this);
+	}
+	
+	private Struct(String structName, String structDefinitionName) {
+		this.dataType = new HashMap<String, DataType>();
+		// TODO recopier les datatype de structDefinitionName dans structName
+		
+		this.dataValue = new HashMap<String, String>();
+		
+		Struct.struct.put(structName, this);
+	}
+
+	public void putValue(String key, String value) {
+		this.dataType.put(key, DataType.VAR);
+		this.dataValue.put(key, value);
+	}
+	
+	public void putPointer(String key, String value) {
+		this.dataType.put(key, DataType.PTR);
+		this.dataValue.put(key, value);
 	}
 	
 	@Override
@@ -31,11 +53,14 @@ public class Struct {
 		// TODO, faire HTML ici
 		// TODO genre avec des UL LUI
 		
-		for (String s : this.data.keySet()) {
-			DataType dt = this.data.get(s);
+		for (String s : this.dataType.keySet()) {
+			DataType dt = this.dataType.get(s);
+			String v = this.dataValue.get(s);
 			
 			if (dt == DataType.VAR) {
 				sb.append(s); // Typiquement, ca serait un LI
+				sb.append(": ");
+				sb.append(v);
 			} else if (dt == DataType.PTR) {
 				sb.append(Struct.getStruct(s).toString()); // et ca un UL dans dans un LI (des UL LI imbrique quoi)
 			}
